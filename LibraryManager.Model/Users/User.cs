97a3c09567f29e87a5718242fc44c1446996e8e2
Oswaldo Beltrani Neto <osvaldo.beltrame.neto@gmail.com>
2024-11-18 00:03:59 -0300

    Inclusão de classes seguindo documentação

---
+++ LibraryManager.Model/Users/User.cs
@@ -0,0 +1,34 @@
+namespace LibraryManager.Model.Users;
+
+public class User
+{
+    public User(string? name, string? email, string? userType, DateTime? registerDate)
+    {
+        Name = name;
+        Email = email;
+        UserType = userType;
+        RegisterDate = registerDate;
+    }
+
+    public int? UserId { get; set; }
+    public string? Name { get; set; }
+    public string? Email { get; set; }
+    public string? UserType { get; set; }
+    public DateTime? RegisterDate { get; set; }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj == null || obj.GetType() !=  GetType())
+        {
+            return false;
+        }
+        var other = (User)obj;
+
+        return base.Equals(UserId.HasValue && other.UserId.HasValue && UserId == other.UserId);
+    }
+
+    public override int GetHashCode()
+    {
+        return UserId.HasValue ? UserId.GetHashCode() : 0;
+    }
+}
